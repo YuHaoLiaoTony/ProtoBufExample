fb5beadc0cc9b32b5be9df4763d8b43170b2de20
Tony <tony.liao@ark88.net>
2022-09-06 11:21:17 +0800

    Proto Example

---
+++ ProtoBufExample/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using ProtoBuf;
+using ProtoBuf.Meta;
+using ProtoBufExample.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ProtoBufExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 輸出 proto octet-stream
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetProto()
+        {
+            List<TestContact> dateMatchs = new List<TestContact>()
+        {
+            new TestContact
+            {
+                ID = 1,
+                Name = "xiao ming",
+                Address = "Cheng Du",
+                Time = DateTime.Now,
+                Datas = new List<TestContact2>{ },
+            },
+            new TestContact
+            {
+                ID = 2,
+                Name = "xiao ming2",
+                Address = "Cheng Du2",
+                Time = DateTime.Now.AddHours(1),
+                Datas = new List<TestContact2>{ },
+            },
+            new TestContact
+            {
+                ID = 3,
+                Name = "xiao ming3",
+                Address = "Cheng Du3",
+                Time = DateTime.Now.AddHours(2),
+                Datas = new List<TestContact2>{ },
+            }
+        };
+            TestResponse response = new TestResponse
+            {
+                DateMatchs = dateMatchs,
+            };
+            byte[] data = null;
+            using (var memoryStream = new MemoryStream())
+            {
+                Serializer.Serialize(memoryStream, response);
+                data = memoryStream.ToArray();
+            }
+
+            return File(data, "application/octet-stream");
+        }
+        /// <summary>
+        /// 測試
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult TestProto()
+        {
+            var client = new RestClient("https://localhost:44381/Home/GetProto");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+
+            byte[] byteArray = response.RawBytes;
+
+            TestResponse testResponse = null;
+            using (MemoryStream fileStream = new MemoryStream(byteArray))
+            {
+                testResponse = Serializer.Deserialize<TestResponse>(fileStream);
+            }
+
+            return Json(testResponse, JsonRequestBehavior.AllowGet);
+        }
+        /// <summary>
+        /// 輸出 Proto 文件
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string GetProtoFile()
+        {
+            return Serializer.GetProto<TestResponse>(ProtoSyntax.Proto3);
+        }
+    }
+}
---
+++ ProtoBufExample/Models/TestContact.cs
@@ -0,0 +1,41 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+
+namespace ProtoBufExample.Models
+{
+    [ProtoContract]
+    public class TestContact
+    {
+        [ProtoMember(1)]
+        public int ID { get; set; }
+        [ProtoMember(2)]
+        public string Name { get; set; }
+        [ProtoMember(3)]
+        public string Address { get; set; }
+        [ProtoMember(4)]
+        public DateTime Time { get; set; }
+        [ProtoMember(5)]
+        public List<TestContact2> Datas { get; set; } = new List<TestContact2>();
+    }
+
+    [ProtoContract]
+    public class TestResponse
+    {
+        [ProtoMember(1)]
+        public IEnumerable<TestContact> DateMatchs { get; set; }
+    }
+
+    [ProtoContract]
+    public class TestContact2
+    {
+        [ProtoMember(1)]
+        public int ID { get; set; }
+        [ProtoMember(2)]
+        public string Name { get; set; }
+        [ProtoMember(3)]
+        public string Address { get; set; }
+        [ProtoMember(4)]
+        public DateTime Time { get; set; }
+    }
+}
